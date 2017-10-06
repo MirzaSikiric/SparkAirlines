c1d658013f7874ef4600e781d7e26fd22485ba7e
Mirza <sikiricmirza@gmail.com>
2017-10-06 22:56:55 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mirza/navigationdrawer/profil.java
@@ -0,0 +1,28 @@
+package com.example.mirza.navigationdrawer;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by mirza on 10/4/17.
+ */
+
+public class profil extends Fragment {
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        getActivity().setTitle("profile");
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.profil, container, false);
+    }
+}
